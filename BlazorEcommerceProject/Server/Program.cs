a56f7819f83668e7e7dff850a855d45186808add
rvill30 <rvill30@wgu.edu>
2023-08-31 15:11:59 -0500

    Product Details, Categories & Seeding More Products

--- BlazorEcommerceProject/Server/Program.cs
+++ BlazorEcommerceProject/Server/Program.cs
@@ -2,6 +2,7 @@ global using BlazorEcommerceProject.Shared;
 global using Microsoft.EntityFrameworkCore;
 global using BlazorEcommerceProject.Server.Data;
 global using BlazorEcommerceProject.Server.Services.ProductService;
+global using BlazorEcommerceProject.Server.Services.CategoryService;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +20,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
