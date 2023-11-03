2b6dc96b7496cb028a9acc0996624a0768d19a07
rvill30 <rvill30@wgu.edu>
2023-11-02 22:07:35 -0500

    Add Product Search Methods to Server and Client

--- BlazorEcommerceProject/Shared/Product.cs
+++ BlazorEcommerceProject/Shared/Product.cs
@@ -1,4 +1,4 @@
-﻿using BlazorEcommerce.Shared;
+﻿using BlazorEcommerceProject.Shared;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -14,8 +14,9 @@ namespace BlazorEcommerceProject.Shared
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2")]
+		public decimal Price { get; set; }
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
-        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
     }
 }
